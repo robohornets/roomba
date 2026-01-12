540f69a641b2027e02ab3cb2a7f4843fe44f84c1
Caedmon Myers <caedmonmyers@icloud.com>
2026-01-12 00:15:13 -0600

    Updates to WhatTime package version and imports.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
@@ -33,7 +34,7 @@ public class Robot extends TimedRobot {
     @Override
     public void robotPeriodic() {
         pdp.clearStickyFaults();
-        
+
         m_timeAndJoystickReplay.update();
         CommandScheduler.getInstance().run();
 
@@ -108,4 +109,9 @@ public class Robot extends TimedRobot {
 
     @Override
     public void simulationPeriodic() {}
+
+
+    public void updateNetworkTablesValues() {
+        // NetworkTablesUtil.put()
+    }
 }
