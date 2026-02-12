64ca9ea25d341c41868921b13a76341730509af0
Caedmon Myers <caedmonmyers@icloud.com>
2026-02-11 18:17:32 -0600

    Updated QuestNav. Fixed error in RobotContainer.java.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -77,7 +77,7 @@ public class RobotContainer {
     // MARK: Xbox Controllers
     public final DriverJoystick driverJoystick = new DriverJoystick(new CommandXboxController(0), drivetrain, questNavSubsystem);
     public final OperatorJoystick operatorJoystick = new OperatorJoystick(new CommandXboxController(1), drivetrain);
-    public final DebugJoystick debugJoystick = new DebugJoystick(new CommandXboxController(2), drivetrain);
+    public final DebugJoystick debugJoystick = new DebugJoystick(new CommandXboxController(2), drivetrain, questNavSubsystem);
 
     
     // MARK: Subsystems
