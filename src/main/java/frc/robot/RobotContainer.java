2aef319f701fb5efbc44a3dbc30d09f98001675e
Caedmon Myers <caedmonmyers@icloud.com>
2026-02-14 16:32:32 -0600

    Updated auto chooser code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,6 +23,7 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -105,7 +106,7 @@ public class RobotContainer {
         registerCommands.registerCommands();
 
         autoChooser = AutoBuilder.buildAutoChooser("Tests");
-        ShuffleboardUtil.put("Auto Chooser", autoChooser);
+        SmartDashboard.putData("Auto Chooser", autoChooser);
 
         // MARK: Run Tests
         /* Disable tests on actual code */
