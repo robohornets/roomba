ac850260133bbeab2f366318a57fe622c0f2c5fa
Tullysaurus <tullypro69@gmail.com>
2026-03-05 18:12:25 +0000

    Fix build to support hyprland arch (bc im cool and use arch and hyprland)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "roomba";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 146;
-    public static final String GIT_SHA = "aed67b9aca058b490c7e477211260716707c116a";
-    public static final String GIT_DATE = "2026-03-04 21:35:15 CST";
+    public static final int GIT_REVISION = 147;
+    public static final String GIT_SHA = "f579082753a45b52b0a87e1582e48a21f433bdb3";
+    public static final String GIT_DATE = "2026-03-05 11:25:00 CST";
     public static final String GIT_BRANCH = "feature/intake-and-shooter";
-    public static final String BUILD_DATE = "2026-03-05 11:23:54 CST";
-    public static final long BUILD_UNIX_TIME = 1772731434178L;
+    public static final String BUILD_DATE = "2026-03-05 12:09:12 CST";
+    public static final long BUILD_UNIX_TIME = 1772734152768L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
