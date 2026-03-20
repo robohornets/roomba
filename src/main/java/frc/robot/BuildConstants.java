ff18854078aaf3ff1872a62e820bdabb2f04b32e
1209robotics <1209robohornets@gmail.com>
2026-03-20 15:10:51 -0500

    Fix jotsling

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "roomba";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 251;
-    public static final String GIT_SHA = "aa307a9fde083f61b41ffe68a28b4a5ea742a4b5";
-    public static final String GIT_DATE = "2026-03-20 14:18:39 CDT";
+    public static final int GIT_REVISION = 253;
+    public static final String GIT_SHA = "287906e559f65e20170878de79d1ad21ba0cf733";
+    public static final String GIT_DATE = "2026-03-20 14:47:28 CDT";
     public static final String GIT_BRANCH = "comp";
-    public static final String BUILD_DATE = "2026-03-20 14:28:19 CDT";
-    public static final long BUILD_UNIX_TIME = 1774034899270L;
+    public static final String BUILD_DATE = "2026-03-20 15:10:43 CDT";
+    public static final long BUILD_UNIX_TIME = 1774037443804L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
--- src/main/java/frc/robot/commands/namedcommands/RegisterCommands.java
+++ src/main/java/frc/robot/commands/namedcommands/RegisterCommands.java
@@ -100,7 +100,7 @@ public class RegisterCommands {
         // MARK: IntakeAgitate
         NamedCommands.registerCommand("IntakeAgitate",
             Commands.repeatingSequence(
-                Commands.runOnce(() -> intakeSubsystem.setPosition(IntakeConstants.INTAKE_MAX_VALUE / 2)),
+                Commands.runOnce(() -> intakeSubsystem.setPosition(0.35)),
                 Commands.waitSeconds(0.75),
                 Commands.runOnce(() -> intakeSubsystem.setPosition(IntakeConstants.INTAKE_MIN_VALUE)),
                 Commands.waitSeconds(0.75)
