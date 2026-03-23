21426eba342204d022a944ac2eb6fad10b98779c
1209robotics <1209robohornets@gmail.com>
2026-03-23 16:36:44 -0500

    Fine tune intake constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "roomba";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 289;
-    public static final String GIT_SHA = "1a5229cf5821634bf533d83c575388199eda7c72";
-    public static final String GIT_DATE = "2026-03-23 16:06:25 CDT";
+    public static final int GIT_REVISION = 292;
+    public static final String GIT_SHA = "9818b3d347ebd0fd5f108208e2e3a4b6e9b4181c";
+    public static final String GIT_DATE = "2026-03-23 16:16:55 CDT";
     public static final String GIT_BRANCH = "comp";
-    public static final String BUILD_DATE = "2026-03-23 16:09:10 CDT";
-    public static final long BUILD_UNIX_TIME = 1774300150462L;
+    public static final String BUILD_DATE = "2026-03-23 16:32:19 CDT";
+    public static final long BUILD_UNIX_TIME = 1774301539790L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/mechanisms/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/mechanisms/intake/IntakeConstants.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems.mechanisms.intake;
 
 public class IntakeConstants {
-    public static final double INTAKE_MIN_VALUE = 0;
+    public static final double INTAKE_MIN_VALUE = 0.1;
     public static final double INTAKE_MAX_VALUE = 2.0;
-    public static final double INTAKE_THRESHOLD = 0.075;
+    public static final double INTAKE_THRESHOLD = 0.1;
 
     public static final double INTAKE_WHEELS_SPEED = 1.0;
     public static final double INTAKE_DOWN_SPEED = 0.4;
