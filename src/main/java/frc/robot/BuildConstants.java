ef623544bc1616a617690daaea52b103e7e7d9f0
1209robotics <1209robohornets@gmail.com>
2026-03-27 11:09:17 -0500

    Add motor logging to shooter subsystem

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "roomba";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 307;
-    public static final String GIT_SHA = "0ebd1f36b93406ef4a543b256dd63f467c371b13";
-    public static final String GIT_DATE = "2026-03-27 10:59:27 CDT";
+    public static final int GIT_REVISION = 308;
+    public static final String GIT_SHA = "66e794c145b873a652b85f3e96e751d579bd5000";
+    public static final String GIT_DATE = "2026-03-27 11:06:01 CDT";
     public static final String GIT_BRANCH = "comp";
-    public static final String BUILD_DATE = "2026-03-27 11:04:16 CDT";
-    public static final long BUILD_UNIX_TIME = 1774627456940L;
+    public static final String BUILD_DATE = "2026-03-27 11:06:49 CDT";
+    public static final long BUILD_UNIX_TIME = 1774627609282L;
     public static final int DIRTY = 1;
 
     private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/mechanisms/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/mechanisms/shooter/ShooterSubsystem.java
@@ -111,6 +111,7 @@ public class ShooterSubsystem extends SubsystemBase {
         // }
 
         logValues();
+        logMotors();
     }
 
     // MARK: Get Pigeon
