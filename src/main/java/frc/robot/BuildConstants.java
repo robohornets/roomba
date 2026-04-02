306f375ad4b5617273b6d72a32c011f6746fd4a6
1209robotics <1209robohornets@gmail.com>
2026-04-02 16:16:58 -0500

    Increase feeder speed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "roomba";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 325;
-    public static final String GIT_SHA = "a8f6cb0de7efb6ffb08d5c1b7b6fa28b4be56db1";
-    public static final String GIT_DATE = "2026-04-02 10:50:16 CDT";
+    public static final int GIT_REVISION = 328;
+    public static final String GIT_SHA = "7812a8c6038e2ef08d546500a01623e6503841f3";
+    public static final String GIT_DATE = "2026-04-02 15:14:13 CDT";
     public static final String GIT_BRANCH = "comp";
-    public static final String BUILD_DATE = "2026-04-02 12:37:01 CDT";
-    public static final long BUILD_UNIX_TIME = 1775151421721L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2026-04-02 15:47:19 CDT";
+    public static final long BUILD_UNIX_TIME = 1775162839115L;
+    public static final int DIRTY = 0;
 
     private BuildConstants(){}
 }
